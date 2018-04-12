8f6b786a475500572159981cb0e259fd0f5ddf97
Oscar Diaz <odiaz.dw@gmail.com>
2018-04-11 23:03:38 -0600

    addded player score

---
+++ src/components/player-card/playerCard.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'player-card',
+  templateUrl: './playerCard.component.html',
+  styleUrls: ['./playerCard.component.css']
+})
+export class PlayerCardComponent {
+  
+}
